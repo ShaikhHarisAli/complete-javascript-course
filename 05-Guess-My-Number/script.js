ef90ed3529f72a483d0d91076f6b11a562863709
shaikhharisali <shaikhharisali16@gmail.com>
2025-05-22 00:29:11 +0500

    Complete Guess My Number Project

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,57 @@
 'use strict';
+// document.querySelector('.message').textContent = '✅ Correct Number';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = '23';
+// document.querySelector('.score').textContent = '15';
+// document.querySelector('.guess').value = '6';
+
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener('click', check);
+document.querySelector('.again').addEventListener('click', again);
+
+function check() {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    displayMessage('☹️ No Number');
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('✅ Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+}
+
+function again() {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+}
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
