7128c113e20e330726e3745a495b2dec95b1dd4e
shaikhharisali <shaikhharisali16@gmail.com>
2025-05-24 15:50:15 +0500

    Complete Project#2 Modal Window

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,6 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+function opemModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', opemModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
